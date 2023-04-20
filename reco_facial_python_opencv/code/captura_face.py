a8905b9d13a4f9e9d59baff7b7bbf7a22a630f6b
@m4rkn4la <alanmarquesec2@gmail.com>
2023-04-19 22:46:22 -0300

    code update
    implementando a captura da face

--- reco_facial_python_opencv/code/captura_face.py
+++ reco_facial_python_opencv/code/captura_face.py
@@ -1,10 +1,17 @@
 import cv2
 
 cam = cv2.VideoCapture(0)
-
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while(True):
     conectado, img = cam.read()
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    detected_faces = classifier.detectMultiScale(gray_img,
+                                                 scaleFactor = 1.5,
+                                                 minSize = (100, 100))
+
+    for (x, y, l, a) in detected_faces:
+        cv2.rectangle(img, (x, y), (x + l), (y + a), (0, 0, 2551), 2)
 
     cv2.imshow('Face', img)
     cv2.waitKey()
